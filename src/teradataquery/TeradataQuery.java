4bc516e4673e8ad12faa475ecbd3f5d80f21567a
Panji Tri Atmojo <panji.atmojo@gmail.com>
2016-02-10 06:41:56 +0700

    Support Multiple Statement Query

--- src/teradataquery/TeradataQuery.java
+++ src/teradataquery/TeradataQuery.java
@@ -10,6 +10,12 @@ package teradataquery;
  * @author Panji Tri Atmojo
  */
 import com.google.gson.Gson;
+import java.awt.List;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.sql.Connection;
@@ -19,6 +25,8 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -34,8 +42,10 @@ public class TeradataQuery {
     public PreparedStatement stmt = null;
     public ResultSet rs = null;
     public HashMap<String, Object> resultArray = new HashMap<>();
+    public HashMap<Integer, Object> resultSet = new HashMap<>();
     public Boolean queryStatus = false;
     public Integer affectedRows = 0;
+    public String[] queryArray = null;
 
     public static void main(String[] args) throws Exception {
 
@@ -52,20 +62,30 @@ public class TeradataQuery {
             /*  get query from supplied args  */
             teradataQuery.getQuery(args);
 
-            teradataQuery.analyze();
-
             /*  create connection  */
             teradataQuery.createConnection();
 
-            /*  prepare statement  */
-            teradataQuery.prepareStatement();
+            /*  break the query into query array    */
+            teradataQuery.breakQuery();
 
-            /*  get results  */
-            teradataQuery.getResult();
+            for (int i = 0; i < teradataQuery.queryArray.length; i++) {
 
-            /*  close connection    */
-            teradataQuery.resultArray.put("status", "OK");
+                teradataQuery.query = teradataQuery.queryArray[i];
 
+                /*  analyze the type of the query   */
+                teradataQuery.analyze();
+
+                /*  prepare statement  */
+                teradataQuery.prepareStatement();
+
+                /*  get results  */
+                teradataQuery.getResult();
+
+                /*  close connection    */
+                teradataQuery.resultArray.put("status", "OK");
+
+                teradataQuery.resultSet.put(i, teradataQuery.resultArray);
+            }
         } catch (Exception e) {
             StringWriter sw = new StringWriter();
             PrintWriter pw = new PrintWriter(sw);
@@ -106,7 +126,7 @@ public class TeradataQuery {
                 this.rs = this.stmt.executeQuery();
                 break;
             default:
-                this.rs = this.stmt.executeQuery();
+                this.queryStatus = this.stmt.execute();
                 break;
         }
 
@@ -130,14 +150,19 @@ public class TeradataQuery {
                 resultMap.put(rowNum, rowResult);
                 rowNum++;
             }
+        } else if (this.queryType.equals("multiple")) {
+            HashMap<String, Object> rowResult = new HashMap<>();
+            rowResult.put("queryStatus", this.queryStatus);
+            resultMap.put(0, rowResult);
         } else {
             HashMap<String, Object> rowResult = new HashMap<>();
             rowResult.put("affectedRows", this.affectedRows);
             resultMap.put(0, rowResult);
-
         }
         /* put the result into resultArray */
         this.resultArray.put("data", resultMap);
+        
+        this.stmt.close();
     }
 
     public void getCredential(String[] args) {
@@ -151,9 +176,44 @@ public class TeradataQuery {
         this.resultArray.put("connectionUrl", this.connurl);
     }
 
-    public void getQuery(String[] args) {
-        this.query = args[3];
-        this.resultArray.put("query", this.query);
+    public void getQuery(String[] args) throws IOException {
+
+        Pattern file = Pattern.compile("^file:(.*)$", Pattern.CASE_INSENSITIVE);
+
+        Matcher m = file.matcher(args[3]);
+
+        if (m.matches()) {
+
+            String fileSource = m.group(1);
+
+            BufferedReader br = null;
+            try {
+                br = new BufferedReader(new FileReader(fileSource));
+
+                StringBuilder sb = new StringBuilder();
+                String line = br.readLine();
+
+                while (line != null) {
+                    sb.append(line);
+                    sb.append(System.lineSeparator());
+                    line = br.readLine();
+                }
+
+                this.query = sb.toString();
+
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(TeradataQuery.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(TeradataQuery.class.getName()).log(Level.SEVERE, null, ex);
+            } finally {
+                if (br != null) {
+                    br.close();
+                }
+            }
+        } else {
+            this.query = args[3];
+        }
+        //this.resultArray.put("query", this.query);
     }
 
     public void closeConn() throws SQLException {
@@ -166,43 +226,41 @@ public class TeradataQuery {
 
         try {
             Gson gson = new Gson();
-            String json = gson.toJson(this.resultArray);
+            String json = gson.toJson(this.resultSet);
             System.out.println(json);
         } catch (Exception e) {
 
         }
     }
 
+    public void breakQuery() {
+        /*  split the query based limit by ";"  */
+        this.queryArray = this.query.split(";");
+
+    }
+
     public void analyze() {
-        String[] queryArray = this.query.split(";");
 
-        if (queryArray.length == 0) {
-            queryArray[0] = this.query;
-        }
-
-        int queryLength = queryArray.length;
-
-        String lastQuery = queryArray[0].toLowerCase();
         String type = "others";
 
-        Pattern select = Pattern.compile("^.*?select.*$");
-        Pattern insert = Pattern.compile("^.*?insert.*$");
-        Pattern update = Pattern.compile("^.*?update.*$");
-        Pattern delete = Pattern.compile("^.*?delete.*$");
+        Pattern select = Pattern.compile("^.*?select.*$", Pattern.CASE_INSENSITIVE);
+        Pattern insert = Pattern.compile("^.*?insert.*$", Pattern.CASE_INSENSITIVE);
+        Pattern update = Pattern.compile("^.*?update.*$", Pattern.CASE_INSENSITIVE);
+        Pattern delete = Pattern.compile("^.*?delete.*$", Pattern.CASE_INSENSITIVE);
 
-        Matcher m = select.matcher(lastQuery);
+        Matcher m = select.matcher(this.query);
         if (m.matches()) {
             type = "select";
         }
-        m = insert.matcher(lastQuery);
+        m = insert.matcher(this.query);
         if (m.matches()) {
             type = "insert";
         }
-        m = update.matcher(lastQuery);
+        m = update.matcher(this.query);
         if (m.matches()) {
             type = "update";
         }
-        m = delete.matcher(lastQuery);
+        m = delete.matcher(this.query);
         if (m.matches()) {
             type = "delete";
         }
